fd5535169cec9c957ee0ce0c91762dac444ed9bc
Maksymilian Kozak <yerbacode@protonmail.com>
2025-02-17 23:14:08 +0100

    feat: add edit functionality

--- src/Components/List.elm
+++ src/Components/List.elm
@@ -1,24 +1,43 @@
-module Components.List exposing (view, Todo)
+module Components.List exposing (Todo, view)
 
-import Html exposing (Html, ul, li, text)
-import Html.Attributes exposing (class)
-import Html.Events exposing (onClick)
+import Html exposing (Html, li, text, ul)
+import Html.Attributes exposing (class, value)
+import Html.Events exposing (onClick, onInput)
+
 
 type alias Todo =
-    { value : String, id: Int
+    { value : String
+    , id : Int
+    , isEditing : Bool
+    , editValue : String
     }
 
-view : List Todo -> (Int -> msg) -> Html msg
-view todos onDelete=
-    ul [ class "space-y-2 w-full" ]
-        (List.map (viewTodoItem onDelete) todos)
 
-viewTodoItem : (Int -> msg) -> Todo -> Html msg
-viewTodoItem onDelete todo  =
+view : List Todo -> (Int -> msg) -> (Int -> msg) -> (Int -> String -> msg) -> Html msg
+view todos onDelete onEdit onUpdateEdit =
+    ul [ class "space-y-2 w-full" ]
+        (List.map (viewTodoItem onDelete onEdit onUpdateEdit) todos)
+
+
+viewTodoItem : (Int -> msg) -> (Int -> msg) -> (Int -> String -> msg) -> Todo -> Html msg
+viewTodoItem onDelete onEdit onUpdateEdit todo =
     li
         [ class "block p-4 rounded-lg bg-gray-800 text-white border border-gray-700 hover:bg-gray-700 transition-colors duration-200" ]
-        [ text todo.value , Html.button
-                [ onClick (onDelete todo.id)
-                , class "px-4 py-2 bg-blue-600 text-white rounded hover:bg-blue-700"
-                ]
-                [ text "Remove" ]  ]
+        [ if todo.isEditing then
+            Html.input [ value todo.editValue, onInput (\newValue -> onUpdateEdit todo.id newValue) ] []
+
+          else
+            text todo.value
+        , Html.button
+            [ onClick (onDelete todo.id)
+            , class "px-4 py-2 bg-blue-600 text-white rounded hover:bg-blue-700"
+            ]
+            [ text "Remove" ]
+        , Html.button [ onClick (onEdit todo.id) ]
+            [ if todo.isEditing then
+                text "Save"
+
+              else
+                text "Edit"
+            ]
+        ]
--- src/Main.elm
+++ src/Main.elm
@@ -1,26 +1,32 @@
 module Main exposing (main)
 
 import Browser
-import Html exposing (Html, div, textarea, button, text)
+import Components.List as TodoList
+import Html exposing (Html, button, div, text, textarea)
 import Html.Attributes exposing (class, placeholder, value)
 import Html.Events exposing (onClick, onInput)
-import Components.List as TodoList
+
 
 type alias Model =
     { currentInput : String
     , todoList : List TodoList.Todo
     }
 
+
 type Msg
     = UpdateInput String
     | AddTodo
     | DeleteTodo Int
+    | EditTodo Int
+    | UpdateEditingTodo Int String
+
 
 init : Model
 init =
     { todoList = []
     , currentInput = ""
     }
+
 
 update : Msg -> Model -> Model
 update msg model =
@@ -30,25 +36,60 @@ update msg model =
 
         AddTodo ->
             { model
-            | todoList = { value = model.currentInput, id = (List.length model.todoList) + 1  } :: model.todoList
-            , currentInput = ""
+                | todoList = { value = model.currentInput, id = List.length model.todoList + 1, isEditing = False, editValue = model.currentInput } :: model.todoList
+                , currentInput = ""
             }
+
         DeleteTodo numberOfItem ->
-            {  model
-            | todoList = List.filter (\todo -> todo.id /= numberOfItem) model.todoList
+            { model
+                | todoList = List.filter (\todo -> todo.id /= numberOfItem) model.todoList
             }
+
+        EditTodo numberOfItem ->
+            { model
+                | todoList =
+                    List.map
+                        (\todo ->
+                            if todo.id == numberOfItem then
+                                if todo.isEditing then
+                                    { todo | isEditing = False, value = todo.editValue }
+
+                                else
+                                    { todo | isEditing = True }
+
+                            else
+                                todo
+                        )
+                        model.todoList
+            }
+
+        UpdateEditingTodo numberOfItem newValue ->
+            { model
+                | todoList =
+                    List.map
+                        (\todo ->
+                            if todo.id == numberOfItem then
+                                { todo | editValue = newValue }
+
+                            else
+                                todo
+                        )
+                        model.todoList
+            }
+
 
 view : Model -> Html Msg
 view model =
     div [ class "flex flex-col items-center gap-4 p-4" ]
-        [ TodoList.view model.todoList DeleteTodo
+        [ TodoList.view model.todoList DeleteTodo EditTodo UpdateEditingTodo
         , div [ class "flex gap-2" ]
             [ textarea
                 [ placeholder "Your TODO note"
                 , value model.currentInput
                 , onInput UpdateInput
                 , class "rounded p-2 bg-gray-800 text-white border border-gray-700"
-                ] []
+                ]
+                []
             , button
                 [ onClick AddTodo
                 , class "px-4 py-2 bg-blue-600 text-white rounded hover:bg-blue-700"
@@ -56,6 +97,7 @@ view model =
                 [ text "Add!" ]
             ]
         ]
+
 
 main : Program () Model Msg
 main =
